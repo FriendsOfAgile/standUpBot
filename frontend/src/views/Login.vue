7599f6d012066c11c93a6faa8131e954a4cab844
atogz <webdevatogz@gmail.com>
2020-04-22 03:56:36 +0300

    auth logos

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -2,7 +2,12 @@
     <div class="w-full h-fullVh flex justify-center items-center">
         <div class="flex flex-col text-center">
             <h3 class="text-xl text-gray-500 font-bold">Get started</h3>
-            <img class="mt-2 cursor-pointer" :src="require('../assets/img/slack-logo.png')" />
+            <div class="w-full flex mt-4 space-x-6">
+                <img class="mt-2 cursor-pointer" :src="require('../assets/img/slack-logo.png')" />
+                <img class="mt-2 cursor-pointer" :src="require('../assets/img/ms-teams.png')" />
+                <img class="mt-2 ml-6 cursor-pointer" :src="require('../assets/img/discord-logo.png')" />
+            </div>
+
         </div>
     </div>
 </template>
