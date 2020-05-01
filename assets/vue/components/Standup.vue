5c72bb890982b0a26df0500bbd6d5b699b11d481
atogz <webdevatogz@gmail.com>
2020-05-01 15:17:52 +0300

    questions handler

--- assets/vue/components/Standup.vue
+++ assets/vue/components/Standup.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="w-full flex flex-col">
+        {{ standUpData.questions }}
         <div class="flex justify-between px-6 pt-6">
             <h3 class="font-bold text-2xl text-gray-700">Standup {{ standUpData.name }}</h3>
             <button class="bg-white text-white border-2 border-gray-500 font-bold py-2 px-4 rounded" :class="[!edited ? 'cursor-not-allowed' : '', !edited ? 'opacity-50' : '', !edited ? 'text-gray-500' : '', edited ? 'bg-accentColor' : '', edited ? 'border-accentColor' : '' ]">
@@ -16,18 +17,18 @@
         <div class="flex flex-col p-6">
             <div class="flex flex-col space-y-4">
                 <label for="beforeMessage" class="text-gray-500">Intro message:
-                    <input id="beforeMessage" class="w-full text-gray-700 border border-gray-500 rounded mt-2 p-3" autofocus type="text" v-model="standUpData.messageBefore" @input="compareConfigs()">
+                    <input id="beforeMessage" class="w-full text-gray-700 border border-gray-500 rounded mt-2 p-3" autofocus type="text" v-model="standUpData.messageBefore" @input="compareConfigs">
                 </label>
                 <label for="afterMessage" class="text-gray-500">Outro message:
-                    <input id="afterMessage" class="w-full text-gray-700 border border-gray-500 rounded mt-2 p-3" type="text" v-model="standUpData.messageAfter" @input="compareConfigs()">
+                    <input id="afterMessage" class="w-full text-gray-700 border border-gray-500 rounded mt-2 p-3" type="text" v-model="standUpData.messageAfter" @input="compareConfigs">
                 </label>
             </div>
             <div class="flex flex-col p-1 mt-2">
                 <h3 class="text-lg font-bold text-gray-700 border-b-4 mt-4 mb-2 border-accentColor w-content">Questions</h3>
                 <div class="flex flex-col ">
-                    <div class="w-full flex-flex-col" v-for="question in standUpData.questions">
+                    <div class="w-full flex-flex-col" v-for="question in standUpData.questions" :key="question.id">
                         <div class="border-l-4 py-1 px-2 ml-4 mt-4 " :style="{'border-color': question.color}">
-                            {{ question.text }}
+                            <input type="text" v-model="question.text" @input="compareConfigs"/>
                         </div>
                     </div>
                 </div>
@@ -53,7 +54,9 @@
       compareConfigs() {
         if(JSON.stringify(this.standUpData) !== JSON.stringify(this.initialStandUpData)) {
           this.edited = true;
-        } else this.edited = false;
+        } else {
+          this.edited = false;
+        }
       },
       editField(fieldRef) {
         this.editFieldShown = fieldRef;
@@ -63,10 +66,12 @@
       if(this.$store.getters.getStandUpConfigs.length) {
         this.initialStandUpData = this.$store.getters.getStandUpConfigData(Number(this.$route.params.id));
         this.standUpData = {...this.initialStandUpData};
+        //this.standUpData.questions.flat();
       } else {
         this.$store.dispatch('GET_STANDUP_CONFIGS').then( () => {
           this.initialStandUpData = this.$store.getters.getStandUpConfigData(Number(this.$route.params.id));
           this.standUpData = {...this.initialStandUpData};
+          //this.standUpData.questions.flat();
         });
       }
     },
